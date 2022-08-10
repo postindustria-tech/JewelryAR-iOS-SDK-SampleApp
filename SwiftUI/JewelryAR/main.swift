4f23a6cc5d73b772b813d35a0adf9d50cd386e9e
Maxim Kucherov <maxim.kucherov@postindustria.com>
2022-08-10 11:34:28 +0300

    Add SwiftUI example.

---
+++ SwiftUI/JewelryAR/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  JewelryAR_Example
+//
+//  Created by Maxim Kucherov on 27.07.2022.
+//
+
+import Foundation
+import JewelryAR
+
+InitArgs(CommandLine.argc, CommandLine.unsafeArgv)
+
+JewelryARApp.main()
